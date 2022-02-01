7f5189148ef03422b9596061c2ab8bbf009fb366
TogrulMustafa <togrulmustafazade8@gmail.com>
2022-02-01 20:39:00 +0400

    Layihe bitirildi

--- js/script.js
+++ js/script.js
@@ -118,3 +118,75 @@ let swiper = new Swiper(".client-swiper", {
 });
 
 // Get-In-Touch ends
+
+
+
+
+
+const scrollTopBtn = document.querySelector('.scrollToTop-btn')
+const scrollDownBtn = document.querySelector('.scroll-down')
+
+window.addEventListener('scroll', _ => {
+    scrollTopBtn.classList.toggle('active', window.scrollY > 500)
+})
+
+scrollTopBtn.addEventListener('click', _ => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+    // ?Niye yalniz body ile yazanda islemir?
+})
+scrollDownBtn.addEventListener('click', _ => {
+    document.body.scrollTo(0, 10000)
+    document.documentElement.scrollTo(0, 10000)
+    // ?Basqa cur nece ede bilerem saytin en sonuna getmek ucun?
+})
+
+
+
+
+
+
+
+window.addEventListener('scroll', _ => {
+    let sections = document.querySelectorAll('section')
+    let scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight
+        let sectionTop = current.offsetTop - 50
+        let id = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(`.nav-items a[href*=${id}]`).classList.add('active')
+        } 
+        else {
+            document.querySelector(`.nav-items a[href*=${id}]`).classList.remove('active')
+        } 
+    })   
+})
+
+
+
+
+
+
+
+
+
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02', {delay: 500, origin: 'left'});
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay: 600, origin: 'right'});
+ScrollReveal().reveal('.home .info .btn', {delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.media-icons i, .contact-left li', {delay: 500, origin: 'left', interval: 200});
+ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.about .description, .contact-right', {delay: 600, origin: 'right'});
+ScrollReveal().reveal('.about .professional-list li', {delay: 500, origin: 'right', interval: 200});
+ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', {delay: 700, origin: 'left'});
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', {delay: 800, origin: 'bottom', interval: 200});
+ScrollReveal().reveal('footer .group', {delay: 500, origin: 'top', interval: 200});
