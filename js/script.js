1ddddf24214d682377ca72a65ba695c85472d6f0
TogrulMustafa <togrulmustafazade8@gmail.com>
2022-02-03 17:30:18 +0400

    Layihenin JS terefinde bezi deyisiklikler  ve elaveler edildi.

--- js/script.js
+++ js/script.js
@@ -128,6 +128,8 @@ const scrollDownBtn = document.querySelector('.scroll-down')
 
 window.addEventListener('scroll', _ => {
     scrollTopBtn.classList.toggle('active', window.scrollY > 500)
+    scrollTopBtn.classList.toggle('passive', window.scrollY > 6345)
+    // ?6345
 })
 
 scrollTopBtn.addEventListener('click', _ => {
@@ -136,15 +138,26 @@ scrollTopBtn.addEventListener('click', _ => {
     // ?Niye yalniz body ile yazanda islemir?
 })
 scrollDownBtn.addEventListener('click', _ => {
-    document.body.scrollTo(0, 10000)
-    document.documentElement.scrollTo(0, 10000)
+    let body = document.querySelector('body')
+    let home = document.querySelector('.home')
+    let homeHeight = home.offsetHeight
+    let bodyHeight = body.offsetHeight
+    document.body.scrollTo(homeHeight, bodyHeight)
+    document.documentElement.scrollTo(homeHeight, bodyHeight)
     // ?Basqa cur nece ede bilerem saytin en sonuna getmek ucun?
 })
 
 
 
 
-
+/* 
+1.pageYOffset- vertical olaraq window-nun 0px-den scroll-u hereket etdirib mueyyen yerde dayandigimiz yere qeder ki mesafeni teyin edir.
+2.offsetHeight- hansi elemente verilibse,onun height-nin,padding-top ve padding-bottom-nun,border-top ve border-bottom-nun cemidir(Margin daxil deyil).
+3.offsetTop- hansi elemente verilibse,onun ana elementi ile onun arasindaki mesafewni teyin edir.Bu mesafeye ovlad elemente verilen top ve margin-top,ana elemente verilen padding-top ve border-top daxildir.
+4.(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)- scrollY xetti sectionun yuxari ve asagi serheddi daxilinde oldugu muddetce hemin sectione active classi tetbiq edilsin serheddi asan kimi
+   active classi hemin sectiondan silinib novbeti sectiona elave edilsin cunki onun serheddine girmisik.
+5.
+*/
 
 
 window.addEventListener('scroll', _ => {
@@ -171,8 +184,6 @@ window.addEventListener('scroll', _ => {
 
 
 
-
-
 ScrollReveal({ 
     reset: true,
     distance: '60px',
@@ -183,10 +194,42 @@ ScrollReveal({
 ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02', {delay: 500, origin: 'left'});
 ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay: 600, origin: 'right'});
 ScrollReveal().reveal('.home .info .btn', {delay: 700, origin: 'bottom'});
-ScrollReveal().reveal('.media-icons i, .contact-left li', {delay: 500, origin: 'left', interval: 200});
+ScrollReveal().reveal('.media-icons i, .contact-left li, .scroll-down', {delay: 500, origin: 'left', interval: 200});
 ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom'});
 ScrollReveal().reveal('.about .description, .contact-right', {delay: 600, origin: 'right'});
 ScrollReveal().reveal('.about .professional-list li', {delay: 500, origin: 'right', interval: 200});
 ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', {delay: 700, origin: 'left'});
 ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', {delay: 800, origin: 'bottom', interval: 200});
 ScrollReveal().reveal('footer .group', {delay: 500, origin: 'top', interval: 200});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener('scroll', _ => {
+//     let header = document.querySelector('header')
+//     let home = document.querySelector('.home')
+//     let about = document.querySelector('.about')
+//     let skills = document.querySelector('.skills')
+//     let services = document.querySelector('.services')
+//     let portfolio = document.querySelector('.portfolio')
+//     let contact = document.querySelector('.contact')
+//     let footer = document.querySelector('footer')
+//     let body = document.querySelector('body')
+
+    
+//     let scrollY = window.pageYOffset
+//     let toptobtn = document.querySelector('.toptobtn')
+
+
+//     toptobtn.innerHTML = scrollY + '<br>' + about.offsetTop + '<br>' + about.offsetHeight
+// })
